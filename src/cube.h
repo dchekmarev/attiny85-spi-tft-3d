7ddfe4bf5dff460afbb740e20f4021b7edb78264
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-17 00:13:16 +0700

    missing files: cube and gpuio

---
+++ src/cube.h
@@ -0,0 +1,86 @@
+#define CUBE_SIZE 64
+
+#define NPOINTS 8
+
+uint16_t points[NPOINTS][2];  // eight 2D points for the cube, values will be calculated in the code
+
+int8_t const orig_points[NPOINTS][3] = {  // eight 3D points - set values for 3D cube
+    {-1, -1, 1},
+    {1, -1, 1},
+    {1, 1, 1},
+    {-1, 1, 1},
+    {-1, -1, -1},
+    {1, -1, -1},
+    {1, 1, -1},
+    {-1, 1, -1}};
+
+float rotated_3d_points[NPOINTS][3];  // eight 3D points - rotated around Y axis
+uint16_t angle_deg_0 = 60;         // rotation around the Y axis
+uint16_t angle_deg_1 = 60;         // rotation around the X axis
+uint16_t angle_deg_2 = 60;         // rotation around the Z axis
+#define z_offset -4.0              // offset on Z axis
+float time_frame;               // ever increasing time value
+
+/**
+ * rotate points around given axis by given degree
+ */
+void rotate(uint16_t angle_deg, uint8_t axis0) {
+  uint8_t axis1 = axis0 == 1 ? 0 : 1;
+  uint8_t axis2 = axis0 == 2 ? 0 : 2;
+  float rads = radians(angle_deg);
+  float cos_val = cos(rads);
+  float sin_val = sin(rads);
+
+  // rotate 3d points in given 2-axis projection
+  for (uint8_t i = 0; i < NPOINTS; i++) {
+    float axis0_coord = rotated_3d_points[i][axis0] * cos_val - rotated_3d_points[i][axis2] * sin_val;
+    float axis1_coord = rotated_3d_points[i][axis1];
+    float axis2_coord = rotated_3d_points[i][axis0] * sin_val + rotated_3d_points[i][axis2] * cos_val;
+    rotated_3d_points[i][axis0] = axis0_coord;
+    rotated_3d_points[i][axis1] = axis1_coord;
+    rotated_3d_points[i][axis2] = axis2_coord;
+  }
+}
+
+void cube_calculate() {
+  time_frame++;
+
+  uint16_t cube_size = (CUBE_SIZE - 15 - 1) + sin(time_frame * 0.2) * 15;
+  angle_deg_0 = (angle_deg_0 + 3) % 360;
+  angle_deg_1 = (angle_deg_1 + 5) % 360;
+  angle_deg_2 = (angle_deg_2 + 7) % 360;
+
+#if DEBUG_ENABLED == 1
+  angle_deg_0 = angle_deg_1 = angle_deg_2 = 0;
+#endif
+
+  // init points
+  for (uint8_t i = 0; i < NPOINTS; i++) {
+    rotated_3d_points[i][0] = orig_points[i][0];
+    rotated_3d_points[i][1] = orig_points[i][1];
+    rotated_3d_points[i][2] = orig_points[i][2];
+  }
+
+  // rotate to current position
+  rotate(angle_deg_0, 0);
+  rotate(angle_deg_1, 1);
+  rotate(angle_deg_2, 2);
+
+  // calculate the points
+  for (uint8_t i = 0; i < NPOINTS; i++) {
+    // project 3d points into 2d space with perspective divide -- 2D x = x/z,   2D y = y/z
+    points[i][0] = round((CUBE_SIZE / 2) + rotated_3d_points[i][0] / (rotated_3d_points[i][2] + z_offset) * cube_size);
+    points[i][1] = round((CUBE_SIZE / 2) + rotated_3d_points[i][1] / (rotated_3d_points[i][2] + z_offset) * cube_size);
+  }
+}
+
+void connectPoints(uint8_t i, uint8_t j, uint16_t points[][2]);
+
+void cube_render() {
+    // connect the lines between the individual points
+    for (uint8_t i = 0; i < 4; i++) {
+      connectPoints(i, (i + 1) % 4, points);
+      connectPoints(4 + i, 4 + (i + 1) % 4, points);
+      connectPoints(i, 4 + i, points);
+    }
+}
---
+++ src/gpu_io.h
@@ -0,0 +1,69 @@
+// void command(uint8_t c) {
+//   // DC_LOW;
+//   digitalWrite(TFT_DC, LOW);
+//   transfer(c);
+// }
+
+// void data(uint8_t c) {
+//   // DC_HIGH;
+//   digitalWrite(TFT_DC, HIGH);
+//   transfer(c);
+// }
+
+#define OUT_REG PORTB
+
+#define DC_HIGH (OUT_REG |= (1 << TFT_DC))
+#define DC_LOW (OUT_REG &= ~(1 << TFT_DC))
+
+void command(uint8_t c) {
+  DC_LOW;
+  transfer(c);
+}
+
+void data(uint8_t c) {
+  DC_HIGH;
+  transfer(c);
+}
+
+void setAddrAndRW_cont(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
+  command(ILI9341_CASET); // Column addr set
+  data(x1 >> 8);
+  data(x1 & 0xFF);     // XSTART
+  data(x2 >> 8);
+  data(x2 & 0xFF);     // XEND
+
+  command(ILI9341_PASET); // Row addr set
+  data(y1 >> 8);
+  data(y1 & 0xFF);     // YSTART
+  data(y2 >> 8);
+  data(y2 & 0xFF);     // YEND
+
+  command(ILI9341_RAMWR); // write to RAM
+}
+
+void fillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t c) {
+#if DEBUG_ENABLED == 1
+  Debug.print(F("fillRect x = "));
+  Debug.print(x);
+  Debug.print(F(", y = "));
+  Debug.print(y);
+  Debug.print(F(", w = "));
+  Debug.print(w);
+  Debug.print(F(", h = "));
+  Debug.print(h);
+  Debug.print(F(", c = "));
+  Debug.println(c);
+#endif
+  w = min(w, SCREEN_WIDTH - x);
+  h = min(h, SCREEN_HEIGHT - y);
+  setAddrAndRW_cont(x, y, x + w - 1, y + h - 1);
+  uint8_t hi = c >> 8, lo = c & 0xFF;
+  digitalWrite(TFT_DC, HIGH);
+  for (int x = 0; x < w; ++x) {
+    for (int y = 0; y < h; ++y) {
+      transfer(hi);
+      transfer(lo);
+    }
+  }
+  digitalWrite(TFT_DC, LOW);
+}
