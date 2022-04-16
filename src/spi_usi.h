7f3d86463b193872870c70fa495a2b851ba5d283
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-16 19:41:24 +0700

    cleanup in spi_usi

--- src/spi_usi.h
+++ src/spi_usi.h
@@ -1,10 +1,7 @@
-#include <tinySPI.h>  // https://github.com/JChristensen/tinySPI
-
-#define OUT_REG PORTB
-#define IN_REG PINB
-
-#define DC_HIGH (OUT_REG |= (1 << TFT_DC))
-#define DC_LOW (OUT_REG &= ~(1 << TFT_DC))
+#define SPI_DDR_PORT DDRB
+#define USCK_DD_PIN DDB2
+#define DO_DD_PIN DDB1
+#define DI_DD_PIN DDB0
 
 void spi_init() {
   pinMode(TFT_DC, OUTPUT);
@@ -18,7 +15,7 @@ void spi_init() {
 void transfer(uint8_t dat) {
   USIDR = dat;
 
-// cycle millis: 420, fps: 2.38 -> cycle millis: 285, fps: 3.51
+// 320x240 full screen fill: cycle millis: 285, fps: 3.51
 
   uint8_t CLK_LOW = (1 << USIWM0) | (1 << USICS1) | (1 << USITC);
   uint8_t CLK_HIGH = (1 << USIWM0) | (1 << USICS1) | (1 << USITC) | (1 << USICLK);
@@ -39,14 +36,4 @@ void transfer(uint8_t dat) {
   USICR = CLK_HIGH;
   USICR = CLK_LOW;
   USICR = CLK_HIGH;
-}
-
-void command(uint8_t c) {
-  DC_LOW;
-  transfer(c);
-}
-
-void data(uint8_t c) {
-  DC_HIGH;
-  transfer(c);
 }
