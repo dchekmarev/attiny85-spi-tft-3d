61f717b2e48689b611df2789a5ce4702bddbe4fc
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-17 20:32:24 +0700

    replace float with int*100 to save space 4486 -> 4128

--- src/cube.h
+++ src/cube.h
@@ -23,16 +23,16 @@ uint16_t time_frame;        // ever increasing time value
 /**
  * rotate point around given axis by given degree
  */
-void rotate(uint16_t angle_deg, uint8_t axis0, float rotated_3d_points[3]) {
+void rotate(uint16_t angle_deg, uint8_t axis0, int16_t rotated_3d_points[3]) {
   // rotate 3d points in given 2-axis projection
   uint8_t axis1 = axis0 == 1 ? 0 : 1;
   uint8_t axis2 = axis0 == 2 ? 0 : 2;
-  float cos_val = cos(radians(angle_deg));
-  float sin_val = sin(radians(angle_deg));
+  int16_t cos_val = (int16_t) (cos(radians(angle_deg)) * 100);
+  int16_t sin_val = (int16_t) (sin(radians(angle_deg)) * 100);
 
-  float axis0_coord = rotated_3d_points[axis0] * cos_val - rotated_3d_points[axis2] * sin_val;
-  float axis1_coord = rotated_3d_points[axis1];
-  float axis2_coord = rotated_3d_points[axis0] * sin_val + rotated_3d_points[axis2] * cos_val;
+  int16_t axis0_coord = (rotated_3d_points[axis0] * cos_val - rotated_3d_points[axis2] * sin_val) / 100;
+  int16_t axis1_coord = rotated_3d_points[axis1];
+  int16_t axis2_coord = (rotated_3d_points[axis0] * sin_val + rotated_3d_points[axis2] * cos_val) / 100;
   rotated_3d_points[axis0] = axis0_coord;
   rotated_3d_points[axis1] = axis1_coord;
   rotated_3d_points[axis2] = axis2_coord;
@@ -52,15 +52,14 @@ void cube_update() {
 
 void cube_calculate() {
   cube_update();
-  uint16_t cube_size = (CUBE_SIZE - 15 - 1) + sin(time_frame * 0.2) * 15;
+  int16_t cube_size = (CUBE_SIZE - 15 - 1) + sin(time_frame * 0.2) * 15;
 
-  float rotated_3d_points[3];  // eight 3D points - rotated around Y axis
-
+  int16_t rotated_3d_points[3];  // eight 3D points - rotated around Y axis
   // init points
   for (uint8_t i = 0; i < NPOINTS; ++i) {
-    rotated_3d_points[0] = orig_points[i][0];
-    rotated_3d_points[1] = orig_points[i][1];
-    rotated_3d_points[2] = orig_points[i][2];
+    rotated_3d_points[0] = orig_points[i][0] * 100;
+    rotated_3d_points[1] = orig_points[i][1] * 100;
+    rotated_3d_points[2] = orig_points[i][2] * 100;
 
     // rotate to current position
     rotate(angle_deg_0, 0, rotated_3d_points);
@@ -70,9 +69,9 @@ void cube_calculate() {
     // calculate the points
 
     // project 3d points into 2d space with perspective divide -- 2D x = x/z,   2D y = y/z
-    float zRatio = rotated_3d_points[2] + z_offset;
-    points[i][0] = (CUBE_SIZE / 2) + (int16_t)(rotated_3d_points[0] * cube_size / zRatio);
-    points[i][1] = (CUBE_SIZE / 2) + (int16_t)(rotated_3d_points[1] * cube_size / zRatio);
+    int16_t zRatio = rotated_3d_points[2] + z_offset * 100;
+    points[i][0] = (CUBE_SIZE / 2) + (rotated_3d_points[0] * cube_size / zRatio);
+    points[i][1] = (CUBE_SIZE / 2) + (rotated_3d_points[1] * cube_size / zRatio);
   }
 }
 
