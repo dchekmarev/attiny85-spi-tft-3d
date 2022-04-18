6b73617a99cad3e2283fcb004a27bcc755c1dc16
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-18 17:58:34 +0700

    use USITC only for triggering

--- src/spi_usi.h
+++ src/spi_usi.h
@@ -5,35 +5,35 @@
 
 void spi_init() {
   pinMode(TFT_DC, OUTPUT);
-  USICR &= ~(_BV(USISIE) | _BV(USIOIE) | _BV(USIWM1));
-  USICR |= _BV(USIWM0) | _BV(USICS1) | _BV(USICLK);
+
   SPI_DDR_PORT |= _BV(USCK_DD_PIN);  // set the USCK pin as output
   SPI_DDR_PORT |= _BV(DO_DD_PIN);    // set the DO pin as output
   SPI_DDR_PORT &= ~_BV(DI_DD_PIN);   // set the DI pin as input
+
+  USICR = (1 << USIWM0) | (1 << USICS1);
 }
 
 // 320x240 full screen fill: cycle millis: 280, fps: 3.57
 
-#define CLK_LOW ((uint8_t) ((1 << USIWM0) | (1 << USICS1) | (1 << USITC)))
-#define CLK_HIGH ((uint8_t) ((1 << USIWM0) | (1 << USICS1) | (1 << USITC) | (1 << USICLK)))
+#define CLK_TOGGLE ((uint8_t) ((1 << USIWM0) | (1 << USICS1) | (1 << USITC)))
 
 void transfer(uint8_t dat) {
   USIDR = dat;
 
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
-  USICR = CLK_LOW;
-  USICR = CLK_HIGH;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
+  USICR = CLK_TOGGLE;
 }
