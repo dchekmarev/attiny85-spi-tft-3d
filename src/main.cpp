699a86929ce3b4e98f8c2088cc1f0cfcc250c917
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-22 19:48:29 +0700

    more adafruit tft init commands

--- src/main.cpp
+++ src/main.cpp
@@ -43,28 +43,44 @@ SoftwareSerial serial(rxPin, txPin);
 
 static const uint8_t init_commands[] PROGMEM =
 {
+  // ref https://github.com/adafruit/Adafruit_ILI9341/blob/master/Adafruit_ILI9341.cpp
     1, ILI9341_SWRESET,
     4, 0xEF, 0x03, 0x80, 0x02,
-    4, 0xCF, 0x00, 0XC1, 0X30,
-    5, 0xED, 0x64, 0x03, 0X12, 0X81,
-    4, 0xE8, 0x85, 0x00, 0x78,
-    6, 0xCB, 0x39, 0x2C, 0x00, 0x34, 0x02,
-    2, 0xF7, 0x20,
-    3, 0xEA, 0x00, 0x00,
-    2, ILI9341_PWCTR1, 0x23, // Power control
-    2, ILI9341_PWCTR2, 0x10, // Power control
+    4, 0xCF, 0x00, 0XC1, 0X30, // 8.4.2 Power control B (CFh)
+    5, 0xED, 0x64, 0x03, 0X12, 0X81, // 8.4.6 Power on sequence control (EDh)
+    4, 0xE8, 0x85, 0x00, 0x78, // 8.4.3 Driver timing control A (E8h)
+    6, 0xCB, 0x39, 0x2C, 0x00, 0x34, 0x02, // 8.4.1 Power control A (CBh)
+    2, 0xF7, 0x20, // 8.4.8 Pump ratio control (F7h)
+    3, 0xEA, 0x00, 0x00, // 8.4.5 Driver timing control B (EAh)
+    2, ILI9341_PWCTR1, 0x23, // Power control, default 0x21 (4.5V), 0x09h is 3.ff
+    2, ILI9341_PWCTR2, 0x10, // Power control, default is 0x10
     3, ILI9341_VMCTR1, 0x3e, 0x28, // VCM control
     2, ILI9341_VMCTR2, 0x86, // VCM control2
     2, ILI9341_MADCTL, 0x48, // Memory Access Control
+    2, ILI9341_VSCRSADD, 0x00,             // Vertical scroll zero
     2, ILI9341_PIXFMT, 0x55,
-    3, ILI9341_FRMCTR1, 0x00, 0x18,
-    4, ILI9341_DFUNCTR, 0x08, 0x82, 0x27, // Display Function Control
+    3, ILI9341_FRMCTR1, 0x00, 0x18, // 8.3.2. Frame Rate Control (In Normal Mode/Full Colors) (B1h)
+    4, ILI9341_DFUNCTR, 0x08, 0x82, 0x27, // Display Function Control, default is 0A,82,27
     2, 0xF2, 0x00, // Gamma Function Disable
     2, ILI9341_GAMMASET, 0x01, // Gamma curve selected
     16, ILI9341_GMCTRP1, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08,
     0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00, // Set Gamma
     16, ILI9341_GMCTRN1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07,
     0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F, // Set Gamma
+    // // Backlight Control
+    // 2, 0xB8, 0x0C, // default 0x0C, 00-0F
+    // 2, 0xB9, 0xCC, // default 0xCC, 00-FF
+    // 2, 0xBA, 0x04, // default 0x04
+    // 2, 0xBB, 0x65, // default 0x65, DTH_ST [3:0]/DTH_MV [3:0]: This parameter is used set the minimum limitation of grayscale threshold value.
+    // 2, 0xBC, 0x44, // default 0x44, DIM1 [2:0]: This parameter is used to set the transition time of brightness level
+    //                //               to avoid the sharp brightness transition on vision. 
+    // 2, 0xBE, 0x0F, // default 0x0F, PWM_DIV [7:0]: PWM_OUT output frequency control. This command is used to adjust
+    //                //               the PWM waveform frequency of PWM_OUT.
+    //                //               The PWM frequency can be calculated by using the following equation. 
+    // 2, 0xBF, 0x00, // default 0x00, LEDPWMPOL: The bit is used to define polarity of LEDPWM signal.
+
+// 8.2.42. Write Content Adaptive Brightness Control (55h) 
+
     0 // 0 indicates last command
 };
 
