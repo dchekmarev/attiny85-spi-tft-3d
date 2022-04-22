4f15815e6c364e6c22f66dcb170c54a09a411f8e
Dmitriy Chekmarev <dzmitryc@gmail.com>
2022-04-22 14:15:25 +0700

    more clear refresh pause

--- src/gpu_io.h
+++ src/gpu_io.h
@@ -16,16 +16,26 @@ void data(uint8_t c) {
   transfer(c);
 }
 
+#define REFRESH_PAUSE_METHOD 1
+
 void refresh_rate_slow() {
-  // command(0xb1); data(0x00); data(0x10);
-  // max possible values: command(0xb5); data(0x7f); data(0x7f); data(0x1f); data(0x1f);
-  command(0xb5); data(0x4f); data(0x7f); data(0x0a); data(0x14);
+#if REFRESH_PAUSE_METHOD == 1
+  // set big porch which causes refresh pause when sum (VFP + VBP) = (0x60 + 0x62) for tjctm24024-spi
+  // max possible values from spec: command(0xb5); data(0x7f); data(0x7f); data(0x1f); data(0x1f);
+  command(0xb5); data(0x62); data(0x62); data(0x02); data(0x02);
+#else
+  // reduce screen height to minimum of 16px, it works fine if there's static header
+  command(ILI9341_DFUNCTR); data(0x08); data(0x82); data(0x01); // Display Function Control, default is 0A,82,27
+#endif
 }
 
 void refresh_rate_normal() {
-  command(0xb5); data(0x02); data(0x02); data(0x0a); data(0x14);
-  // default: command(0xb1); data(0x00); data(0x1b);
-  // command(0xb1); data(0x00); data(0x10);
+#if REFRESH_PAUSE_METHOD == 1
+  // command(0xb5); data(0x02); data(0x02); data(0x0a); data(0x14); // default
+  command(0xb5); data(0x02); data(0x02); data(0x02); data(0x02);
+#else
+  command(ILI9341_DFUNCTR); data(0x08); data(0x82); data(0x27); // Display Function Control, default is 0A,82,27
+#endif
 }
 
 void setAddrAndRW_cont(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
